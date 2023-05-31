ae36d8f121307e4af32491d7d9a3e23fe0d92325
Danny Han <cool.huip@gmail.com>
2023-05-31 17:52:23 -0400

    Comment out old attempt

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
 
-pygame.init()
-dis = pygame.display.set_mode((400,300))
-pygame.display.update()
-pygame.display.set_caption("coolhuip_snake_game_v0.1")
+# pygame.init()
+# dis = pygame.display.set_mode((400,300))
+# pygame.display.update()
+# pygame.display.set_caption("coolhuip_snake_game_v0.1")
 
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        print(event)    # prints out all the actions that take place on screen
+# game_over = False
+# while not game_over:
+#     for event in pygame.event.get():
+#         print(event)    # prints out all the actions that take place on screen
 
-pygame.quit()
-quit()
+# pygame.quit()
+# quit()
+
