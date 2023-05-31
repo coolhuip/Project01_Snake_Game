5f1217309fd8c1fd3f6a664ae3ebf2c675d0d068
Danny Han <cool.huip@gmail.com>
2023-05-31 18:13:53 -0400

    Define variables

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ import pygame   # Needed to develop the game.
 import time     # Time manipulation.
 import random   # Generate objects randomly.
 
+# Speed of snake
+snake_speed = 15
+
+# Window size
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 460
+
+# Colors
+midnight_blue = pygame.Color(25, 25, 112)
+mint_cream = pygame.Color(245, 255, 250)
+crimson_red = pygame.Color(220, 20, 60)
+lawn_green = pygame.Color(124, 252, 0)
+orange_red = pygame.Color(255, 69, 0)
+
+
 
 
