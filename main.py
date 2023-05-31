26b03abcf8f436caa2deb72725ddff1ac4599c9f
Danny Han <cool.huip@gmail.com>
2023-05-31 18:05:46 -0400

    Start fresh

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 import pygame
 
-# pygame.init()
-# dis = pygame.display.set_mode((400,300))
-# pygame.display.update()
-# pygame.display.set_caption("coolhuip_snake_game_v0.1")
-
-# game_over = False
-# while not game_over:
-#     for event in pygame.event.get():
-#         print(event)    # prints out all the actions that take place on screen
-
-# pygame.quit()
-# quit()
-
